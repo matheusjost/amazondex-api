01d3f264780c948ae6a7ef95da38c107d31ea2c1
Matheus Jost <matheusfjost@outlook.com>
2024-11-23 00:02:26 -0300

    fix: forgot a param

--- src/main/java/br/unisc/amazondex/pojo/BiologiaReprodutivaDTO.java
+++ src/main/java/br/unisc/amazondex/pojo/BiologiaReprodutivaDTO.java
@@ -1,7 +1,9 @@
 package br.unisc.amazondex.pojo;
 
+import br.unisc.amazondex.enums.BiologiaReprodutivaEnum;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public interface BiologiaReprodutivaDTO extends ComboDTO {
+    BiologiaReprodutivaEnum getTipo();
 }
